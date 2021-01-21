3d82ad98aeb1bb20bca60dab4d613b333e521838
Sooho-Kim <kshorg@naver.com>
2021-01-21 23:55:01 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -224,7 +224,7 @@ def encoding_character(english_character):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     morse_code_dict = get_morse_code_dict()
-    result = morse_code_dict[english_character]
+    result = morse_code_dict[english_character.upper()]
     return result
     # ==================================
 
@@ -285,7 +285,7 @@ def encoding_sentence(english_sentence):
         if i == '':
             result += ' '
         else:
-            result += encoding_character(i.upper()) 
+            result += encoding_character(i) 
     return result
     # ==================================
 
